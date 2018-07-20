be5532e6de847b2180edf05053fd6593ca299262
devinMcArthur <assortedgerm@gmail.com>
2018-07-19 18:04:12 -0600

    Forget password functionality

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  $('#password-form, #confirm-form').on('keyup', function () {
+  $('#password-form, #confirm-form, #first-name-form, #last-name-form, #email-form').on('keyup', function () {
     if (($('#first-name-form').val() != '' && $('#last-name-form').val() != '' && $('#email-form').val() != '' && $('#password-form').val() != '' && $('#confirm-form').val() != '') && ($('#password-form').val() == $('#confirm-form').val())) {
       $('#submit-button').removeClass('disabled');
       $('#confirm-icon').removeClass('open');
--- server/server.js
+++ server/server.js
@@ -158,6 +158,118 @@ app.post('/signup', async (req, res) => {
   }
 });
 
+// GET /forgot
+app.get('/forgot', (req, res) => {
+  res.render('user/forgot');
+});
+
+// POST /forgot
+app.post('/forgot', (req, res, next) => {
+  async.waterfall([
+    function(done) {
+      crypto.randomBytes(20, function(err, buf) {
+        var token = buf.toString('hex');
+        done(err, token);
+      });
+    },
+    function(token, done) {
+      User.findOne({ email: req.body.email }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'No account with that email address exists.');
+          return res.redirect('/forgot');
+        }
+        user.resetPasswordToken = token;
+        user.resetPasswordExpires = Date.now() + 3600000; // 1 hour
+        user.save(function(err) {
+          done(err, token, user);
+        });
+      });
+    },
+    function(token, user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: 'app98138005@heroku.com',
+          pass: 'z2ilzs7v6175'
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@bowmark.ca',
+        subject: 'Bow Mark Password Reset',
+        text: 'You are receiving this because you (or someone else) have requested the reset of the password for your account.\n\n' +
+          'Please click on the following link, or paste this into your browser to complete the process:\n\n' +
+          'http://' + req.headers.host + '/reset/' + token + '\n\n' +
+          'If you did not request this, please ignore this email and your password will remain unchanged.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('info', 'An e-mail has been sent to ' + user.email + ' with further instructions.');
+        done(err, 'done');
+      });
+    }
+  ], function(err) {
+    if (err) return next(err);
+    res.redirect('/forgot');
+  });
+});
+
+// GET /reset/:token
+app.get('/reset/:token', (req, res) => {
+  User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+    if (!user) {
+      req.flash('error', 'Password reset token is invalid or has expired.');
+      return res.redirect('/forgot');
+    }
+    res.render('users/reset', {
+      token: req.params.token
+    });
+  });
+});
+
+// POST /reset/:token
+app.post('/reset/:token', function(req, res) {
+  async.waterfall([
+    function(done) {
+      User.findOne({ resetPasswordToken: req.params.token, resetPasswordExpires: { $gt: Date.now() } }, function(err, user) {
+        if (!user) {
+          req.flash('error', 'Password reset token is invalid or has expired.');
+          return res.redirect('back');
+        }
+        user.password = req.body.password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        user.save(function(err) {
+          req.logIn(user, function(err) {
+            done(err, user);
+          });
+        });
+      });
+    },
+    function(user, done) {
+      var smtpTransport = nodemailer.createTransport({
+        service: 'SendGrid',
+        auth: {
+          user: 'app98138005@heroku.com',
+          pass: 'z2ilzs7v6175'
+        }
+      });
+      var mailOptions = {
+        to: user.email,
+        from: 'passwordreset@bowmark.ca',
+        subject: 'Your password has been changed',
+        text: 'Hello,\n\n' +
+          'This is a confirmation that the password for your account ' + user.email + ' has just been changed.\n'
+      };
+      smtpTransport.sendMail(mailOptions, function(err) {
+        req.flash('success', 'Success! Your password has been changed.');
+        done(err);
+      });
+    }
+  ], function(err) {
+    res.redirect('/');
+  });
+});
+
 app.listen(port, () => {
   console.log(`Started on port ${port}`);
 });
