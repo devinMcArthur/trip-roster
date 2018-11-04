5d517c9f79cffc41aa3d204f54ff0b14bf64e382
devinMcArthur <assortedgerm@gmail.com>
2018-11-04 16:31:19 -0400

    Test Podcast RSS Feed URL

--- server/server.js
+++ server/server.js
@@ -15,7 +15,7 @@ const nodemailer = require('nodemailer');
 const async = require('async');
 const flash = require('express-flash');
 const moment = require('moment');
-const xoauth2 = require('xoauth2');
+const Podcast = require('podcast')
 
 const { User } = require('./models/user');
 const { Team } = require('./models/team');
@@ -98,6 +98,72 @@ app.use(function (req, res, next) {
     }
   }
   next();
+});
+
+// Podcast RSS Feed
+app.get('/podcast/feed', async (req, res) => {
+  try {
+    const feed = new Podcast({
+      title: 'Stories From The Nish',
+      description: 'description',
+      feed_url: 'https://www.triproster.com/podcast/feed',
+      site_url: 'http://instagram.com/',
+      image_url: 'http://example.com/icon.png',
+      docs: 'http://example.com/rss/docs.html',
+      author: 'Dylan Greene',
+      managingEditor: 'Dylan Greene',
+      webMaster: 'Dylan Greene',
+      copyright: '2013 Dylan Greene',
+      language: 'en',
+      categories: ['Category 1', 'Category 2', 'Category 3'],
+      pubDate: 'May 20, 2012 04:00:00 GMT',
+      ttl: '60',
+      itunesAuthor: 'Max Nowack',
+      itunesSubtitle: 'I am a sub title',
+      itunesSummary: 'I am a summary',
+      itunesOwner: { name: 'Max Nowack', email: 'max@unsou.de' },
+      itunesExplicit: false,
+      itunesCategory: {
+        "text": "Entertainment",
+        "subcats": [{
+          "text": "Television"
+        }]
+      },
+      itunesImage: 'http://link.to/image.png'
+    });
+
+
+    /* loop over data and add to feed */
+    feed.addItem({
+      title: 'item title',
+      description: 'use this for the content. It can include html.',
+      url: 'http://example.com/article4?this&that', // link to the item
+      guid: '1123', // optional - defaults to url
+      categories: ['Category 1', 'Category 2', 'Category 3', 'Category 4'], // optional - array of item categories
+      author: 'Guest Author', // optional - defaults to feed author property
+      date: 'May 27, 2012', // any format that js Date can parse.
+      lat: 33.417974, //optional latitude field for GeoRSS
+      long: -111.933231, //optional longitude field for GeoRSS
+      // enclosure: { url: '...' }, // optional enclosure
+      itunesAuthor: 'Max Nowack',
+      itunesExplicit: false,
+      itunesSubtitle: 'I am a sub title',
+      itunesSummary: 'I am a summary',
+      itunesDuration: 12345,
+      itunesKeywords: ['javascript', 'podcast']
+    });
+
+    // cache the xml to send to clients
+    const xml = feed.buildXml();
+    console.log(xml);
+
+    res.set('Content-Type', 'text/xml');
+    res.send(xml);
+  } catch (e) {
+    console.log(e);
+    req.flash('error', e.message);
+    res.redirect('/');
+  }
 });
 
 // GET root
